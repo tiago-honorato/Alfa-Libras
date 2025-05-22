a2d16626ae0f0a90d17298ca42b2f13921c3b4e5
tiago <tiagohirute@gmail.com>
2025-05-22 18:41:42 -0300

    BoardData created

---
+++ Alfa-Libras/Assets/Scripts/GameUtility.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameUtility : MonoBehaviour
+{
+
+    public void LoadScene(string sceneName)
+    {
+
+        SceneManager.LoadScene(sceneName);
+
+    }
+
+}
