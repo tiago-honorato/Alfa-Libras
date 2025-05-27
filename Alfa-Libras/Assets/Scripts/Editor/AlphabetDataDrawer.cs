0c538437f365beb9a9ecd1c9f634c6bbc00fdc66
tiago <tiagohirute@gmail.com>
2025-05-27 19:04:22 -0300

    editing alphabetDataDrawer

--- Alfa-Libras/Assets/Scripts/Editor/AlphabetDataDrawer.cs
+++ Alfa-Libras/Assets/Scripts/Editor/AlphabetDataDrawer.cs
@@ -1,16 +1,48 @@
 using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
 
-public class AlphabetDataDrawer : MonoBehaviour
+[CustomEditor(typeof(AlphabetData))]
+[CanEditMultipleObjects]
+[System.Serializable]
+public class AlphabetDataDrawer : Editor
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private ReorderableList AlphabetPlainList;
+    private ReorderableList AlphabetNormalList;
+    private ReorderableList AlphabetHighlightedList;
+    private ReorderableList AlphabetWrongList;
+
+    private void OnEnable()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnInspectorGUI()
     {
         
+    }
+
+    private void InitializeReordableList(ref ReorderableList list, string propertyName, string listLabel)
+    {
+        list = new ReorderableList(serializedObject, serializedObject.FindProperty(propertyName), true, true, true, true);
+
+        list.drawHeaderCallback = (Rect rect) =>
+        {
+            EditorGUI.LabelField(rect, listLabel);
+
+        };
+
+        var l = list;
+
+        list.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+        {
+            var element = l.serializedProperty.GetArrayElementAtIndex(index);
+            rect.y += 2;
+
+            EditorGUI.PropertyField(new Rect(rect.x, rect.y, 60, EditorGUIUtility.singleLineHeight), element.FindPropertyRelative("letter"), GUIContent.none);
+
+            //EditorGUI.PropertyField(new Rect(rect.x + 70, rect.y, )) TO DO
+
+        };
     }
 }
