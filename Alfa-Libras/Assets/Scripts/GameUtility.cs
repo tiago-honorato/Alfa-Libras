c28c555ad35fa5686e368a9aeb9f51884d41bb8c
tiago <tiagohirute@gmail.com>
2025-12-16 18:08:37 -0300

    videos scene almost complete

--- Alfa-Libras/Assets/Scripts/GameUtility.cs
+++ Alfa-Libras/Assets/Scripts/GameUtility.cs
@@ -11,13 +11,50 @@ public class GameUtility : MonoBehaviour
 
     }
 
-    public void LoadSceneByProgression(string categoryName)
+    [Header("Configuração de Progresso")]
+    public string[] categoriesToCheck;
+
+    public void LoadSceneByProgression()
     {
 
-        int currentIndex = DataSaver.ReadCategoryCurrentIndexValues(categoryName);
-        Debug.Log("valor atual: " + currentIndex);
+        int finishedCount = 0;
 
+        foreach (string categoryName in categoriesToCheck)
+        {
+            int status = DataSaver.ReadCategoryCurrentIndexValues(categoryName);
 
+            Debug.Log(categoryName +"/"+status);
+
+            if (status != -1)
+            finishedCount += status;
+        }
+
+        switch(finishedCount)
+        {
+            case 5:
+                SceneManager.LoadScene("SelectVideos");
+                break;
+            case 10:
+                SceneManager.LoadScene("SelectVideos2");
+                break;
+            case 13:
+                SceneManager.LoadScene("SelectVideos3");
+                break;
+            case 16:
+                SceneManager.LoadScene("SelectVideos4");
+                break;
+            case 20:
+                SceneManager.LoadScene("SelectVideos5");
+                break;
+            case 22:
+                SceneManager.LoadScene("SelectVideos6");
+                break;
+            default:
+                Debug.Log("ERRO! Scene não encontrada(GameUtility)");
+                break;
+        }
+
+        Debug.Log("Contador: " + finishedCount);
     }
 
     public void ExitApplication()
